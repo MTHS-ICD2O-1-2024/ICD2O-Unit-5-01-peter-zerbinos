d37f61532bbd8a961cf0e230b1451dd91ca65669
Peter Zerbinos <peter.zerbinos@mths.ca>
2025-05-05 01:58:43 +0000

    added changes to index.html & script.js & style.css

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Peter Zerbinos All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Peter Zerbinos
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+const randomNumber = Math.floor(Math.random() * 6) + 1
+
+// eslint-disable-next-line no-unused-vars
+function checkNumber() {
+  const userNumber = parseInt(document.getElementById('user-number').value)
+
+  if (userNumber === randomNumber) {
+    document.getElementById('result').innerHTML =
+      '<p>Correct! The number was: ' + randomNumber + '.</p>'
+  }
+  
+    if (userNumber !== randomNumber) {
+    document.getElementById('result').innerHTML =
+      '<p>Wrong! The number was: ' + randomNumber + '.</p>'
+  }
+}
